cc5bf2f6235656695c5a0342de3b376f0401e669
EroUsr99 <github@mx.eronet.cloud>
2025-03-12 17:35:57 +0100

    Update swagger-initializer.js

--- dist/5b8zr/macadam/swagger-initializer.js
+++ dist/5b8zr/macadam/swagger-initializer.js
@@ -1,15 +1,9 @@
 window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // Define the Swagger YAML URL for Macadam only
-  const swaggerUrl = "https://api-docs.poweredbyams.com/5b8zr/macadam/";
-
-  // Load the correct Swagger file
   window.ui = SwaggerUIBundle({
-    url: swaggerUrl,
+    url: window.location.href + "swagger.yaml", // Ensures it appends the file correctly
     dom_id: '#swagger-ui',
     deepLinking: true,
-    defaultModelsExpandDepth: 1, // Expands the schema by default
+    defaultModelsExpandDepth: 1,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
@@ -18,16 +12,12 @@ window.onload = function() {
       SwaggerUIBundle.plugins.DownloadUrl
     ],
     layout: "StandaloneLayout",
-    configUrl: "",
     showExtensions: false,
     showCommonExtensions: false
   });
 
-  // Remove top black bar and server label after UI loads
   setTimeout(() => {
     document.querySelector(".topbar")?.remove();
     document.querySelector(".servers")?.parentElement?.remove();
   }, 500);
-
-  //</editor-fold>
 };
